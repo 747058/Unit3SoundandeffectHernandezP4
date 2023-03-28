52b3854e3ae3166dee343864ad81042482be8ef0
747058 <747058@student.dusd.net>
2023-03-28 11:42:21 -0700

    woked on it
    added obstacle

--- Unit3Hernandez/Assets/Scripts/PlayerControl.cs
+++ Unit3Hernandez/Assets/Scripts/PlayerControl.cs
@@ -7,6 +7,7 @@ public class PlayerControl : MonoBehaviour
     private Rigidbody playerRb;
     public float jumpForce;
     public float gravityModifier;
+    public bool isOnGround = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,9 +18,15 @@ public class PlayerControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = true; 
         }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
     }
 }
